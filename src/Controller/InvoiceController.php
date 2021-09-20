e5f52880043a295417060c2142945653ec62e514
Bastien <bastien.munck@me.com>
2021-09-20 09:09:05 +0200

    invoice

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -2,17 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Snappy\Pdf;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class InvoiceController extends AbstractController
 {
     #[Route('/invoice', name: 'invoice')]
-    public function index(): Response
+    public function index(Pdf $knpSnappyPdf): Response
     {
-        return $this->render('invoice/template.html.twig', [
+        $html = $this->renderView('invoice/template.html.twig');
+
+        return new PdfResponse(
+            $knpSnappyPdf->getOutputFromHtml($html),
+            'file.pdf'
+        );
+        /* return $this->render('invoice/template.html.twig', [
             'controller_name' => 'InvoiceController',
-        ]);
+        ]);*/
     }
 }
